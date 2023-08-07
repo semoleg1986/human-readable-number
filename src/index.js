c87de6f08fcdffaaac267427a2d08accb38eb5f6
semoleg1986 <semoleg1986@gmail.com>
2023-08-07 10:38:56 +0300

    feat: method 2

--- src/index.js
+++ src/index.js
@@ -1,69 +1,13 @@
 module.exports = function toReadable (number) {
-  const basicNumtoStr = {
-    0: "zero",
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine",
-    10: "ten",
-    11: "eleven",
-    12: "twelve",
-    13: "thirteen",
-    14: "fourteen",
-    15: "fifteen",
-    16: "sixteen",
-    17: "seventeen",
-    18: "eighteen",
-    19: "nineteen",
-    20: "twenty",
-    30: "thirty",
-    40: "forty",
-    50: "fifty",
-    60: "sixty",
-    70: "seventy",
-    80: "eighty",
-    90: "ninety",
-  }
-  let str = String(number)
-  let res = ''
+  const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+  'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-  const simpleNumber = (num) => {
-    return basicNumtoStr[num]
-  }
-
-  const lessHundredNumber = (num) => {
-    if (num[1]==0){
-      return basicNumtoStr[num]
-    }
-    if (num < 21) {
-      return simpleNumber(num);
-    }
-    return `${basicNumtoStr[num[0] + 0]} ${simpleNumber(num[1])}`
-  }
-
-  const moreHundredNumber = (num) => {
-    if (num[1] == 0 && num[2] == 0){
-      return `${simpleNumber(num[0])} hundred`
-    }
-    if (num[1] == 0){
-      return `${simpleNumber(num[0])} hundred ${simpleNumber(num[2])}`
-    }
-    return `${simpleNumber(num[0])} hundred ${lessHundredNumber(num[1] + num[2])}`
-  }
-
-    if (number<21){
-      res = simpleNumber(str)
-    }
-    else if (number<100){
-      res = lessHundredNumber(str)
-    }
-    else {
-      res = moreHundredNumber(str)
-    }
-  return res
+return number < 20
+? units[number]
+: number < 100
+    ? (number % 10 === 0) ? tens[Math.floor(number / 10)] : `${tens[Math.floor(number / 10)]} ${units[number % 10]}`
+    : number < 1000
+        ? (number % 100 === 0) ? `${units[Math.floor(number / 100)]} hundred` : `${units[Math.floor(number / 100)]} hundred ${toReadable(number % 100)}`
+        : "Number out of range";
 }
